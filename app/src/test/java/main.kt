e1f0afc93a6073db97d5d114df87e9fb0963f996
Gzandonai <giovanni_zandonai@hotmail.com>
2022-09-06 13:27:22 -0300

    file man

--- app/src/test/java/main.kt
+++ app/src/test/java/main.kt
@@ -1,6 +1,12 @@
 package br.com.alura.bytebank.modelo
 
+import teste.testaContasDiferentes
+
 fun main() {
     println("Bem vindo ao ByteBank !")
     println(" ")
+
+    testaContasDiferentes()
+
+
 }
