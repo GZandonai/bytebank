d7e536d71831e2340b67dfc896e3fa1aff215a53
GZandonai <giovanni_zandonai@hotmail.com>
2022-10-06 21:20:49 -0300

    Reduce e Fold

---
+++ app/src/test/java/br/com/alura/bytebank/Arrays/FuncoesBigDecimal.kt
@@ -0,0 +1,25 @@
+package br.com.alura.bytebank.br.com.alura.bytebank.Arrays
+
+import java.math.BigDecimal
+
+
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    return Array<BigDecimal>(valores.size) { i ->
+        valores[i].toBigDecimal()
+    }
+}
+
+
+fun Array<BigDecimal>.somatoria(): BigDecimal {
+    return this.reduce { acumulador, valor ->
+        acumulador + valor
+        }
+}
+
+fun Array<BigDecimal>.media(): BigDecimal{
+    return if (this.isEmpty()) {
+        BigDecimal.ZERO
+    }else{
+        this.somatoria() / this.size.toBigDecimal()
+    }
+}
--- app/src/test/java/br/com/alura/bytebank/Arrays/mainArrays.kt
+++ app/src/test/java/br/com/alura/bytebank/Arrays/mainArrays.kt
@@ -24,29 +24,44 @@ fun main() {
     }
     println(menorIdade)
 
+
     var salarios = bigDecimalArrayOf("1500.55", "2000.0", "5000.00", "10000.00")
     println(salarios.contentToString())
 
     var aumento = "1.1".toBigDecimal()
     val salariosComAumento: Array<BigDecimal> = salarios
-        .map { salario ->
-            if(salario < "5000".toBigDecimal()){
-                salario + "500".toBigDecimal()
-            }  else {
-                (salario * aumento).setScale(2, RoundingMode.UP) }
-            }.toTypedArray()
+        .map { salario -> calculaAumentoRelativo(salario, aumento) }.toTypedArray()
 
     println(salariosComAumento.contentToString())
 
+    val gastoInicial = salariosComAumento.somatoria()
+    println(gastoInicial)
 
-
-
-}
-
-fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
-    return Array<BigDecimal>(valores.size) { i ->
-        valores[i].toBigDecimal()
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acumulador, salario ->
+        acumulador + (salario * meses).setScale(2, RoundingMode.UP)
     }
+    println(gastoTotal)
 
 
+    val media = salariosComAumento
+        .sorted().
+        takeLast(3).
+        toTypedArray().
+        media()
+    println(media)
 }
+
+private fun calculaAumentoRelativo(
+    salario: BigDecimal,
+    aumento: BigDecimal,
+) = if (salario < "5000".toBigDecimal()) {
+    salario + "500".toBigDecimal()
+} else {
+    (salario * aumento).setScale(2, RoundingMode.UP)
+}
+
+
+
+
+
